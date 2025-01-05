b9f1a441db85b91a83304c4dda1b825733d7d1fb
fabiolunghi <f.lunghi96@gmail.com>
2025-01-05 17:55:59 +0100

    feat: add if FizzBuzz

--- style.js
+++ style.js
@@ -4,7 +4,10 @@
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
 for (let i=1; i<=100; i++){
-
+    
+if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+}
 }
 
 
